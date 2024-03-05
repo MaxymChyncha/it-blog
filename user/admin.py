9215d0608b850d20dd4860732507e751291555f6
MaxymChyncha <max.chyncha@gmail.com>
2024-03-05 18:54:03 +0200

    feat(user): register user model to admin panel, unregister groups

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.register(get_user_model(), UserAdmin)
+
+admin.site.unregister(Group)
