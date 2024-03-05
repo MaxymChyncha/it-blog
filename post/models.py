b17649c430419d8a0ec5ea91fa217eb5642ee958
MaxymChyncha <max.chyncha@gmail.com>
2024-03-05 19:00:42 +0200

    feat(post): implement post models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+    tag = models.ManyToManyField(
+        to=Tag,
+        related_name="posts"
+    )
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return f"Title: {self.title}. Author: {self.author.username}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Content: {self.content[:10]}. Author: {self.author.username}"
