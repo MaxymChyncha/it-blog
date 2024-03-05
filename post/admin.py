3c431b8a58446c978d94dbb9ef8ef71664c539dd
MaxymChyncha <max.chyncha@gmail.com>
2024-03-05 19:01:25 +0200

    feat(post): register post models to admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Tag, Post, Comment
+
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
