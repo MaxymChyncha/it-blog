133a1492b5146d55902e4e865a3f73f3b27699e9
MaxymChyncha <max.chyncha@gmail.com>
2024-03-06 16:44:04 +0200

    style(post): refactor according to flake8

--- post/urls.py
+++ post/urls.py
@@ -15,17 +15,61 @@ from post.views import (
 )
 
 urlpatterns = [
-    path("posts/", PostListView.as_view(), name="post-list"),
-    path("posts/create/", PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
-    path("tags/", TagListView.as_view(), name="tag-list"),
-    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
-    path("tags/<int:pk>/", TagDetailView.as_view(), name="tag-detail"),
-    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
-    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
-    path("comments/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete")
+    path(
+        "posts/",
+        PostListView.as_view(),
+        name="post-list"
+    ),
+    path(
+        "posts/create/",
+        PostCreateView.as_view(),
+        name="post-create"
+    ),
+    path(
+        "posts/<int:pk>/",
+        PostDetailView.as_view(),
+        name="post-detail"
+    ),
+    path(
+        "posts/<int:pk>/update/",
+        PostUpdateView.as_view(),
+        name="post-update"
+    ),
+    path(
+        "posts/<int:pk>/delete/",
+        PostDeleteView.as_view(),
+        name="post-delete"
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag-list"
+    ),
+    path(
+        "tags/create/",
+        TagCreateView.as_view(),
+        name="tag-create"
+    ),
+    path(
+        "tags/<int:pk>/",
+        TagDetailView.as_view(),
+        name="tag-detail"
+    ),
+    path(
+        "tags/<int:pk>/update/",
+        TagUpdateView.as_view(),
+        name="tag-update"
+    ),
+    path(
+        "tags/<int:pk>/delete/",
+        TagDeleteView.as_view(),
+        name="tag-delete"
+    ),
+    path(
+        "comments/<int:pk>/delete/",
+        CommentDeleteView.as_view(),
+        name="comment-delete"
+    )
 ]
 
 app_name = "post"
