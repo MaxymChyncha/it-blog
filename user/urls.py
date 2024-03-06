130bdc208b8b1f5c51432312d27df5be2d39b54b
MaxymChyncha <max.chyncha@gmail.com>
2024-03-06 16:04:14 +0200

    feat(user): implement basic user views

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path, include
 
+from user.views import (
+    UserListView,
+    UserCreateView,
+    UserDetailView,
+    UserUpdateView,
+    UserDeleteView
+)
+
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("", UserListView.as_view(), name="user-list"),
+    path("create/", UserCreateView.as_view(), name="user-create"),
+    path("<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("<int:pk>/update/", UserUpdateView.as_view(), name="user-update"),
+    path("<int:pk>/delete/", UserDeleteView.as_view(), name="user-delete"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from config.utils.paginators import paginate_queryset
+from user.forms import UserCreateForm, UserUpdateForm
+
+
+class UserListView(generic.ListView):
+    model = get_user_model()
+    paginate_by = 5
+
+
+class UserCreateView(generic.CreateView):
+    model = get_user_model()
+    form_class = UserCreateForm
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class UserDetailView(generic.DetailView):
+    model = get_user_model()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = self.object.posts.all()
+        paginated_posts = paginate_queryset(self.request, posts, 5)
+        context["posts"] = paginated_posts
+        context["paginator"] = paginated_posts.paginator
+        context["page_obj"] = paginated_posts
+        context["is_paginated"] = paginated_posts.has_other_pages()
+        return context
+
+
+class UserUpdateView(generic.UpdateView):
+    model = get_user_model()
+    form_class = UserUpdateForm
+
+    def get_success_url(self):
+        return self.object.get_absolute_url()
+
+
+class UserDeleteView(generic.DeleteView):
+    model = get_user_model()
+
+    def get_success_url(self):
+        return reverse("user:user-list")
