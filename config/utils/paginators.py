ce0724cddac892a9b6ed537d7df975a08159e9c2
MaxymChyncha <max.chyncha@gmail.com>
2024-03-06 18:31:32 +0200

    refactor(user/post/UI): refactor custom pagination

--- config/utils/paginators.py
+++ config/utils/paginators.py
@@ -1,7 +1,12 @@
 from django.core.paginator import Paginator
 
 
-def paginate_queryset(request, queryset, count):
+def paginate_context(request, queryset, context, count):
     paginator = Paginator(queryset, count)
     page_number = request.GET.get('page')
-    return paginator.get_page(page_number)
+    paginated_object = paginator.get_page(page_number)
+    context["paginated_obj"] = paginated_object
+    context["paginator"] = paginated_object.paginator
+    context["page_obj"] = paginated_object
+    context["is_paginated"] = paginated_object.has_other_pages()
+    return context
