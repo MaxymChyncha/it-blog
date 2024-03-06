797f7f2e6e7d574be94774a5cf3c715748cbf1c9
MaxymChyncha <max.chyncha@gmail.com>
2024-03-06 16:04:14 +0200

    feat(user): implement basic user views

--- user/models.py
+++ user/models.py
@@ -1,9 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class User(AbstractUser):
     class Meta:
         ordering = ("username",)
+
+    @property
+    def posts_counter(self):
+        return self.posts.count()
+
+    @property
+    def comments_counter(self):
+        return self.comments.count()
+
+    def get_absolute_url(self):
+        return reverse("user:user-detail", kwargs={"pk": self.pk})
 
     def __str__(self) -> str:
         return f"{self.username} ({self.first_name} {self.last_name})"
