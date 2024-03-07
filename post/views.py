6b7a382f877f3fb4d1f5d2cb2d1431b951d12302
MaxymChyncha <max.chyncha@gmail.com>
2024-03-07 20:42:42 +0200

    style(user/post): add annotations

--- post/views.py
+++ post/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from django.views import generic
 from django.views.generic.edit import FormMixin
@@ -18,7 +20,7 @@ class PostListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
     template_name = "post/post_list.html"
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = super(PostListView, self).get_queryset()
         queryset = (
             queryset
@@ -32,7 +34,7 @@ class PostListView(LoginRequiredMixin, generic.ListView):
             )
         return queryset
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super(PostListView, self).get_context_data(**kwargs)
         title = self.request.GET.get("title")
         context["search_form"] = PostSearchForm(
@@ -45,14 +47,12 @@ class PostCreateView(LoginRequiredMixin, generic.CreateView):
     model = Post
     form_class = PostCreateForm
 
-    # fields = ("tag", "title", "content",)
-
-    def form_valid(self, form):
+    def form_valid(self, form: PostCreateForm):
         form.instance.author = self.request.user
         form.save()
         return super(PostCreateView, self).form_valid(form)
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return self.object.get_absolute_url()
 
 
@@ -60,7 +60,7 @@ class PostDetailView(LoginRequiredMixin, FormMixin, generic.DetailView):
     model = Post
     form_class = CommentCreateForm
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = super(PostDetailView, self).get_queryset()
         queryset = (
             queryset
@@ -69,7 +69,7 @@ class PostDetailView(LoginRequiredMixin, FormMixin, generic.DetailView):
         )
         return queryset
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super(PostDetailView, self).get_context_data(**kwargs)
         comments = self.object.comments.all()
         context = paginate_context(self.request, comments, context, 5)
@@ -79,7 +79,7 @@ class PostDetailView(LoginRequiredMixin, FormMixin, generic.DetailView):
         )
         return context
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         self.object = self.get_object()
         form = self.get_form()
 
@@ -87,13 +87,13 @@ class PostDetailView(LoginRequiredMixin, FormMixin, generic.DetailView):
             return self.form_valid(form)
         return self.form_invalid(form)
 
-    def form_valid(self, form):
+    def form_valid(self, form: CommentCreateForm):
         form.instance.author = self.request.user
         form.instance.post = self.get_object()
         form.save()
         return super(PostDetailView, self).form_valid(form)
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return self.object.get_absolute_url()
 
 
@@ -101,14 +101,14 @@ class PostUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Post
     fields = ("title", "content",)
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return self.object.get_absolute_url()
 
 
 class PostDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Post
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return reverse("post:post-list")
 
 
@@ -116,7 +116,7 @@ class TagListView(LoginRequiredMixin, generic.ListView):
     model = Tag
     paginate_by = 5
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = super(TagListView, self).get_queryset()
         form = TagSearchForm(self.request.GET)
         if form.is_valid():
@@ -125,7 +125,7 @@ class TagListView(LoginRequiredMixin, generic.ListView):
             )
         return queryset
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super(TagListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name")
         context["search_form"] = TagSearchForm(
@@ -138,14 +138,14 @@ class TagCreateView(LoginRequiredMixin, generic.CreateView):
     model = Tag
     fields = ("name",)
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return reverse("post:tag-list")
 
 
 class TagDetailView(LoginRequiredMixin, generic.DetailView):
     model = Tag
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict:
         context = super(TagDetailView, self).get_context_data(**kwargs)
         posts = self.object.posts.all()
         context = paginate_context(self.request, posts, context, 5)
@@ -156,14 +156,14 @@ class TagUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Tag
     fields = ("name",)
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return reverse("post:tag-list")
 
 
 class TagDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Tag
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return reverse("post:tag-list")
 
 
@@ -171,12 +171,12 @@ class CommentUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Comment
     form_class = CommentUpdateForm
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return self.object.get_absolute_url()
 
 
 class CommentDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Comment
 
-    def get_success_url(self):
+    def get_success_url(self) -> str:
         return self.object.get_absolute_url()
