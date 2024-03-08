9c3ffad79a5466787bc905d688fa97e6eb69eea2
MaxymChyncha <max.chyncha@gmail.com>
2024-03-08 14:55:42 +0200

    refactor(core): add it-blog-matrix.onrender.com to ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
-    "localhost"
+    "localhost",
+    "it-blog-matrix.onrender.com"
 ]
 
 INTERNAL_IPS = [
